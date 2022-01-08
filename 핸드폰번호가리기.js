778a638b834a4f18b3cc9bf8a79693b9cbafe2e1
hwangsebin-2 <sbsbin99@gmail.com>
2022-01-08 23:45:52 +0900

    핸드폰번호가리기 success

--- 핸드폰번호가리기.js
+++ 핸드폰번호가리기.js
@@ -1,1 +1,6 @@
 /* commit test */
+function solution(phone_number) {
+    var answer = phone_number.length -4;
+    return '*'.repeat(answer) + phone_number.substring(answer);
+    //return answer;
+}
